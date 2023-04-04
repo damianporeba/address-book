458dab346a7ec839380030118715e5cc3755b7f3
Damian Poręba <damianporeba98@gmail.com>
2023-04-04 21:21:35 +0200

    added writeline

--- address book/Program.cs
+++ address book/Program.cs
@@ -8,8 +8,11 @@ namespace addressbook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to adress book app!");
-            Console.WriteLine("Please let me know what do you want do:" +
-                "sss");
+            Console.WriteLine("Please let me know what do you want do:");
+            Console.WriteLine("1. Add a new number");
+            Console.WriteLine("2. Remove number");
+            Console.WriteLine("3. Check a number");
+            Console.WriteLine("Check 1, 2 or 3...");
         }
 
     }
