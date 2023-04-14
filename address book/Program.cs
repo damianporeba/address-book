f16f955514e3403bf4e68437e923ed984ffd9536
Damian Poręba <damianporeba98@gmail.com>
2023-04-14 22:06:40 +0200

    added ItemType enum class

--- address book/Program.cs
+++ address book/Program.cs
@@ -9,7 +9,7 @@ namespace addressbook
             Console.WriteLine("Welcome to adress book app!");
             Console.WriteLine("Please let me know what do you want do:");
             Console.WriteLine("1. Add a new number");
-            Console.WriteLine("2. Remove number");
+            Console.WriteLine("2. Remove number");  
             Console.WriteLine("3. Check a number");
             Console.WriteLine("Check 1, 2 or 3...");
 
